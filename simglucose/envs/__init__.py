6d5510ef53a516c8f1094298483f073da6d99797
Taku Yamagata <taku.yamagata@bristol.ac.uk>
2020-02-27 21:37:47 +0000

    On branch clean Changes to be committed: 	modified:   simglucose/envs/__init__.py 	modified:   simglucose/envs/simglucose_gym_env.py
    Change gym wrapper to support new gym

--- simglucose/envs/__init__.py
+++ simglucose/envs/__init__.py
@@ -1,1 +1,6 @@
-from simglucose.envs.simglucose_gym_env import T1DSimEnv
+from gym.envs.registration import register
+
+register(
+    id='simGlucose-v0',
+    entry_point='simglucose.envs.simglucose_gym_env:T1DSimEnv'
+    )
--- simglucose/envs/simglucose_gym_env.py
+++ simglucose/envs/simglucose_gym_env.py
@@ -1,4 +1,5 @@
 from simglucose.simulation.env import T1DSimEnv as _T1DSimEnv
+# from simglucose.patient.t1dpatientApprox import T1DPatient
 from simglucose.patient.t1dpatient import T1DPatient
 from simglucose.sensor.cgm import CGMSensor
 from simglucose.actuator.pump import InsulinPump
@@ -22,24 +23,27 @@ class T1DSimEnv(gym.Env):
     '''
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, patient_name=None, reward_fun=None):
+    def __init__(self, patient_name=None, reward_fun=None, observe_internal_state=False, squash_action=False):
         '''
         patient_name must be 'adolescent#001' to 'adolescent#010',
         or 'adult#001' to 'adult#010', or 'child#001' to 'child#010'
         '''
-        seeds = self._seed()
+        seeds = self.seed()
         # have to hard code the patient_name, gym has some interesting
         # error when choosing the patient
         if patient_name is None:
             patient_name = 'adolescent#001'
         patient = T1DPatient.withName(patient_name)
-        sensor = CGMSensor.withName('Dexcom', seed=seeds[1])
+        sensor = CGMSensor.withName('GuardianRT', seed=seeds[1])
         hour = self.np_random.randint(low=0.0, high=24.0)
-        start_time = datetime(2018, 1, 1, hour, 0, 0)
+        # start_time = datetime(2018, 1, 1, hour, 0, 0) # random start time
+        start_time = datetime(2018, 1, 1, 6, 0, 0) # fixed start time (6am)
         scenario = RandomScenario(start_time=start_time, seed=seeds[2])
         pump = InsulinPump.withName('Insulet')
+        self.observe_internal_state = observe_internal_state
         self.env = _T1DSimEnv(patient, sensor, pump, scenario)
         self.reward_fun = reward_fun
+        self.squash_action = squash_action
 
     @staticmethod
     def pick_patient():
@@ -62,19 +66,47 @@ class T1DSimEnv(gym.Env):
 
             return select
 
-    def _step(self, action):
+    def step(self, action):
         # This gym only controls basal insulin
-        act = Action(basal=action, bolus=0)
-        if self.reward_fun is None:
-            return self.env.step(act)
+        if self.squash_action:
+            basal_rate = 0.0136
+            if action < 0:
+                basal_ = np.exp(action)
+            else:
+                basal_ = 5.0 - (5.0 - 1.0)*np.exp(-1.0/(5.0-1.0)*action)
+            basal_ = basal_ * basal_rate
         else:
-            return self.env.step(act, reward_fun=self.reward_fun)
+            basal_ = action
+        
+        act = Action(basal=basal_, bolus=0)
+        if self.reward_fun is None:
+            step = self.env.step(act)
+        else:
+            step = self.env.step(act, reward_fun=self.reward_fun)
+        
+        if self.observe_internal_state:
+            # observation includes all internal states
+            observation = np.append(step.observation.CGM, step.info['patient_state'])
+        else:
+            observation = np.array(step.observation.CGM)
+        return observation, step.reward, step.done, step.info
 
-    def _reset(self):
-        obs, _, _, _ = self.env.reset()
-        return obs
+    def reset(self):
+        # new scenario
+        seeds = self.seed()
+        hour = self.np_random.randint(low=0.0, high=24.0)
+        # start_time = datetime(2018, 1, 1, hour, 0, 0) # random start time
+        start_time = datetime(2018, 1, 1, 6, 0, 0) # fixed start time (6am)
+        self.env.scenario = RandomScenario(start_time=start_time, seed=seeds[2])
+        
+        obs, _, _, info = self.env.reset()
+        if self.observe_internal_state:
+            observation = np.append(obs.CGM, info['patient_state'])
+        else:
+            observation = np.array(obs.CGM)
+        return observation
 
-    def _seed(self, seed=None):
+    def seed(self, seed=None):
         self.np_random, seed1 = seeding.np_random(seed=seed)
         # Derive a random seed. This gets passed as a uint, but gets
         # checked as an int elsewhere, so we need to keep it below
@@ -83,14 +115,19 @@ class T1DSimEnv(gym.Env):
         seed3 = seeding.hash_seed(seed2 + 1) % 2**31
         return [seed1, seed2, seed3]
 
-    def _render(self, mode='human', close=False):
+    def render(self, mode='human', close=False):
         self.env.render(close=close)
 
     @property
     def action_space(self):
         ub = self.env.pump._params['max_basal']
-        return spaces.Box(low=0, high=ub, shape=(1,))
+        return spaces.Box(low=-np.inf, high=+np.inf, shape=(1,))
+        #return spaces.Box(low=0, high=ub, shape=(1,))
 
     @property
     def observation_space(self):
-        return spaces.Box(low=0, high=np.inf, shape=(1,))
+        if self.observe_internal_state:
+            return spaces.Box(low=0, high=np.inf, shape=(14,))
+        else:
+            return spaces.Box(low=0, high=np.inf, shape=(1,))
+        
